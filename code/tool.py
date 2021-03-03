0937c05bc50deaddde3375774c08acbc7dbace94
suryasiriki4 <cs18b035@iittp.ac.in>
2021-03-03 10:17:30 +0530

    implemeted functions for extracting questions from stackoverflow api

--- code/tool.py
+++ code/tool.py
@@ -86,3 +86,60 @@ def get_search_results(soup):
 
     for result in soup.find_all("div", class_="question-summary search-result"):
         search_results.append(result)
+
+
+
+def get_answers_to_questions(question_ids):
+    """ retriving the most voted anwers and the accepted answers for the question with given ids """
+
+    answers = []
+
+    for question_id in question_ids:
+
+        response = requests.get(ANSWERS_URL.replace("<id>", question_id))
+        items = response.json()["items"]
+
+        if items == []:
+            continue
+
+        most_voted_answer = items[0]
+
+        body_of_most_voted_answer = most_voted_answer["body"]
+
+        markdown_answer_body = html2text(body_of_most_voted_answer)
+
+        answers.append(
+            Answer(
+                id=str(most_voted_answer["answer_id"]),
+                accepted=most_voted_answer["is_accepted"],
+                score=most_voted_answer["score"],
+                body=markdown_answer_body,
+                author=most_voted_answer["owner"]["display_name"],
+                profile_image=most_voted_answer["owner"].get("profile_image", None),
+        ))
+
+    return answers
+
+
+def store_questions(question_ids):
+    questions = []
+
+    for question_id in question_ids:
+
+        response = requests.get(QUESTIONS_URL.replace("<id>", question_id))
+        items = response.json()["items"]
+
+        if items == []:
+            continue
+
+        question = items[0]
+
+        question_body = question["body"]
+
+        markdown_question_body = html2text(question_body)
+
+        questions.append(Question(id=question_id, has_accepted=None, body=markdown_question_body))
+
+
+
+    return questions
