f9d240d0d69d3a5dcbe706b95992846f204c3179
nagarjunaKolloju <cs18b018@iittp.ac.in>
2021-03-03 16:51:37 +0530

    Filtering top questions from google search using BeautifulSoup Module

--- code/tool.py
+++ code/tool.py
@@ -1,12 +1,24 @@
-# regular expression
-# install numpy
-# python3 -c "import nltk; nltk.download('punkt')"
-import re
-import sys
-import os
-
-from bs4 import BeautifulSoup
-import requests
-import googlesearch
-from html2text import html2text
 import random
+
+def souper(url):
+    """ getting the beautifulSoup object from url """
+
+    try:
+        html = requests.get(url, headers={"User-Agent": random.choice(USER_AGENTS)})
+    except requests.exceptions.RequestException:
+        print("unable to fetch stack overflow results")
+        sys.exit(1)
+
+    print(html.url)
+
+    if re.search("\.com/nocaptcha", html.url): # checking if URL is captcha page
+        return None
+    else:
+        return BeautifulSoup(html.text, "html.parser")
+
+def get_search_results(soup):
+    """ return a list of directories of containg serach resluts """
+    search_results = []
+
+    for result in soup.find_all("div", class_="question-summary search-result"):
+        search_results.append(result)
