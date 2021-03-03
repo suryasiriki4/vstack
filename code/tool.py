1a660e72e4547716596f1ef0712d4ff9d445b92f
nagarjunaKolloju <cs18b018@iittp.ac.in>
2021-03-03 17:07:18 +0530

    added extractive text summarization

--- code/tool.py
+++ code/tool.py
@@ -1,5 +1,9 @@
 import random
 
+
+import sumy
+
+
 def souper(url):
     """ getting the beautifulSoup object from url """
 
@@ -22,3 +26,39 @@ def get_search_results(soup):
 
     for result in soup.find_all("div", class_="question-summary search-result"):
         search_results.append(result)
+
+
+
+def print_results(QUSTION_IDS, QUESTIONS, ANSWERS):
+
+    num_of_results = len(QUSTION_IDS)
+
+
+
+    
+    for i in range(num_of_results):
+        answer = ANSWERS[i].body
+        parser = PlaintextParser.from_string(answer,Tokenizer("english"))
+    
+        summarizer = LexRankSummarizer()
+        summary = summarizer(parser.document,10);
+        
+
+
+        print(' ')
+        print("#####################################################################################################")
+        print(' ')
+        print('Question: ')
+        print(' ')
+        print(QUESTIONS[i].body)
+        print(' ')
+        print("-----------------------------------------------------------------------------------------------------")
+        print(' ')
+        print("Author: " + ANSWERS[i].author)
+        print(' ')
+        print(' ')
+        print("Answer: ")
+        for sentence in summary :
+            print(sentence)
+        
+    return
