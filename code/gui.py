6ab336f60684d0a19c49f5102a8bb42d1b28a745
suryasiriki4 <cs18b035@iittp.ac.in>
2021-03-18 12:50:30 +0530

    added ui components

--- code/gui.py
+++ code/gui.py
@@ -3,6 +3,8 @@ from tkinter.scrolledtext import ScrolledText
 import sys
 from tool import fun
 
+ques = []
+answers = []
 
 def search():
 
@@ -18,17 +20,42 @@ def search():
     # insert data
     search_label["text"] = " Searching :{}".format(search_data)
     print("1\n")
-    text.insert(0.0, answersStore[0][0])
-    print("1\n")
+
+    return answersStore
 
 
 def enter_pressed(event):
-    search()
+    answersStore = search()
+    global ques
+    ques = [answer[0] for answer in answersStore]
+    global answers
+    answers = [answer[2] for answer in answersStore]
+    
+    populate_list(ques, parts_list)
+
+
+# table of questions part
+
+def populate_list(ques, parts_list):
+    i = 0
+    for que in ques:
+        i+=1
+        parts_list.insert(END, str(i) + ". " + que)
+
+def view(root, selected_tuple, ques, answers):
+
+    text.delete(0.0, END)
+
+    if len(selected_tuple) == 0:
+        messagebox.showinfo("not selected", "not selected")
+    else:
+        text.insert(0.0, "QUESTION : \n" + ques[selected_tuple[0]] + "\n")
+        text.insert(END, 39 * "*" + "\nANSER : \n" + answers[selected_tuple[0]])
 
 
 root = Tk()
 root.title("TOOL")
-root.geometry("320x450")
+root.geometry("320x550")
 root.configure(bg="white")
 
 ent = StringVar()
@@ -41,20 +68,28 @@ search_entry.place(x=25, y=20)
 search_label = Label(root, text="Searching : ", font=("arial", 12, "bold"))
 search_label.place(x=25, y=70)
 
-text = ScrolledText(root, font=("times", 10), bd=4, relief=SUNKEN, wrap=WORD)
-text.place(x=15, y=100, height=300, width=300)
-
 
 search_button = Button(root, text="Search", width=6, font=(
     "arial", 12, "bold"), command=search)
-search_button.place(x=15, y=400)
+search_button.place(x=15, y=100)
 
-clear_button = Button(root, text="Clear", width=6, font=(
-    "arial", 12, "bold"), command=lambda: text.delete(0.0, END))
-clear_button.place(x=115, y=400)
+view_button = Button(root, text="view", width=6, font=(
+    "arial", 12, "bold"), command=lambda: view(root, parts_list.curselection(), ques, answers))
+view_button.place(x=115, y=100)
 
 exit_button = Button(root, text="Exit", width=6, font=(
     "arial", 12, "bold"), command=root.quit)
-exit_button.place(x=215, y=400)
+exit_button.place(x=215, y=100)
+
+
+
+parts_list = Listbox(root, height=10, width=140)
+parts_list.place(x=15, y=150, height=100, width=300)
+
+
+
+
+text = ScrolledText(root, font=("times", 10), bd=4, relief=SUNKEN, wrap=WORD)
+text.place(x=15, y=250, height=300, width=300)
 
 root.mainloop()
